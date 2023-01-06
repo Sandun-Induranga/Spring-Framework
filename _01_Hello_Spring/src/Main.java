bc6841dbfd2ffa60e06f960bb54d5d24902b7a4d
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-06 10:07:46 +0530

    Content Updated

---
+++ _01_Hello_Spring/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
