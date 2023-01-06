1250a6c1bb1a59290407f30500a2a561a0fac1d4
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-06 11:09:19 +0530

    Content Updated

---
+++ _01_Hello_Spring/src/lk/ijse/spring/pojo/DBConnection.java
@@ -0,0 +1,21 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Component
+public class DBConnection {
+
+    public DBConnection() {
+        System.out.println("DBConnection: Instantiated");
+    }
+
+    public void testConnection(){
+        System.out.println("Connection Received");
+    }
+
+}
