14a0cc266e02e2dcafb124fc82aff7e2defe7c58
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-06 10:24:17 +0530

    Pojo Class Created

--- _01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
+++ _01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -8,7 +9,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  **/
 public class AppInitializer {
     public static void main(String[] args) {
+
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
         ctx.close();
+
     }
 }
---
+++ _01_Hello_Spring/src/lk/ijse/spring/pojo/PojoOne.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Component
+public class PojoOne {
+
+    public PojoOne() {
+        System.out.println("PojoOne: Instantiated");
+    }
+
+}
