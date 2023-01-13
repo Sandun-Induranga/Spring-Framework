f6a57cd8b7b8ecd1d35b5b3fb0d44d49f46c874c
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-13 09:28:19 +0530

    Spring Bean Life Cycle

---
+++ _02_Bean_Life_Cycle/src/lk/ijse/spring/pojo/PojoOne.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Component
+public class PojoOne {
+}
