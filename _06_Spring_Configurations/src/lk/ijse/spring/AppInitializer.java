c888d7eb4692727508038e087b7c1535cc7a14eb
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-13 14:26:14 +0530

    Spring Configurations

---
+++ _06_Spring_Configurations/src/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring;
+
+import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.PojoOne;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+        PojoOne pojoOne = ctx.getBean(PojoOne.class);
+
+        ctx.registerShutdownHook();
+
+    }
+}
