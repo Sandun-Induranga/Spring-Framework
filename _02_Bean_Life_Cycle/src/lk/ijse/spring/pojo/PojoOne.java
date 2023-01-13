919ec14c806e1aa3e41ae88c60ebb5433f641b68
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-13 09:39:17 +0530

    Spring Bean Life Cycle Bean Initializing

--- _02_Bean_Life_Cycle/src/lk/ijse/spring/pojo/PojoOne.java
+++ _02_Bean_Life_Cycle/src/lk/ijse/spring/pojo/PojoOne.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ import org.springframework.stereotype.Component;
  **/
 
 @Component
-public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware {
+public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean {
 
     public PojoOne() {
         System.out.println("PojoOne: Instantiate");
@@ -22,16 +23,21 @@ public class PojoOne implements BeanNameAware, BeanFactoryAware, ApplicationCont
 
     @Override
     public void setBeanName(String name) {
-        System.out.println("pojoOne: BeanName Aware : " + name);
+        System.out.println("PojoOne: BeanName Aware : " + name);
     }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("pojoOne: Bean Factory Aware: ");
+        System.out.println("PojoOne: Bean Factory Aware: ");
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("pojoOne: Application Context Aware");
+        System.out.println("PojoOne: Application Context Aware");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("PojoOne: Initializing Bean: Bean is ready");
     }
 }
