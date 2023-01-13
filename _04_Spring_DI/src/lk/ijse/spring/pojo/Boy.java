7c4e1e5e4d8619f21cf61ab0c79e5d1ffc27031f
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-13 11:15:44 +0530

    Spring DI @Autowired

--- _04_Spring_DI/src/lk/ijse/spring/pojo/Boy.java
+++ _04_Spring_DI/src/lk/ijse/spring/pojo/Boy.java
@@ -1,6 +1,10 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,7 +13,7 @@ import org.springframework.stereotype.Component;
  **/
 
 @Component
-public class Boy {
+public class Boy implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
     @Autowired
     Girl girl;
@@ -18,9 +22,33 @@ public class Boy {
         System.out.println("Boy: Instantiated");
     }
 
-    public void chatWithGirl(){
+    public void chatWithGirl() {
 //        Girl girl = new Girl();    // Don't create objects
         girl.chat();
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Boy: Bean Factory Aware");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Boy: Bean Name Aware");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Boy: Bean Destroyed");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Boy: Bean Initialize: Ready for use");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Boy: Application Context Aware");
+    }
 }
--- _04_Spring_DI/src/lk/ijse/spring/pojo/Girl.java
+++ _04_Spring_DI/src/lk/ijse/spring/pojo/Girl.java
@@ -1,5 +1,9 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,7 +12,7 @@ import org.springframework.stereotype.Component;
  **/
 
 @Component
-public class Girl {
+public class Girl implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
     public Girl() {
         System.out.println("Girl: Instantiated");
@@ -16,5 +20,30 @@ public class Girl {
 
     public void chat(){
         System.out.println("Hello");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Girl: Bean Factory Aware");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Girl: Bean Name Aware");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Girl: Bean Destroyed");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Girl: Bean Initialize: Ready for use");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Girl: Application Context Aware");
     }
 }
