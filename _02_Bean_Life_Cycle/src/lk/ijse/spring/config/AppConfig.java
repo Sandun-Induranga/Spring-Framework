e7a07f1107c1a98c97a2c077e9a95fe24e2efdad
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-13 10:01:39 +0530

    Spring Bean Life Cycle PojoThree

--- _02_Bean_Life_Cycle/src/lk/ijse/spring/config/AppConfig.java
+++ _02_Bean_Life_Cycle/src/lk/ijse/spring/config/AppConfig.java
@@ -1,7 +1,11 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.pojo.PojoThree;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
 
 /**
  * @author : Sandun Induranga
@@ -15,4 +19,10 @@ public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig: Instantiate");
     }
+
+    @Bean
+    public PojoThree pojoThree(){
+        return new PojoThree();
+    }
+
 }
