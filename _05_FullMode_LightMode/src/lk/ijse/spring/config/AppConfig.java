d6a5589260e1f757b902a20d2a52b26780a5dfb7
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-13 13:33:49 +0530

    FullMode LightMode

---
+++ _05_FullMode_LightMode/src/lk/ijse/spring/config/AppConfig.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Configuration
+@ComponentScan("lk.ijse.spring")
+public class AppConfig {
+}
---
+++ _05_FullMode_LightMode/src/lk/ijse/spring/pojo/PojoOne.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Component
+public class PojoOne {
+
+    public PojoOne() {
+        System.out.println("PojoOne: Instantiated");
+    }
+}
