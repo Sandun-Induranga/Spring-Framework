e24c2af3c90e8d334b0dfbe37d5698d26301b8b2
Sandun Induranga <sandutharu40@gmail.com>
2023-01-20 09:49:35 +0530

    Spring web MVC

---
+++ _09_Spring_Web_MVC/src/lk/ijse/spring/pojo/SpringBeanOne.java
@@ -0,0 +1,46 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Component
+public class SpringBeanOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+
+    public SpringBeanOne() {
+        System.out.println("SpringBeanOne: Instantiate");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("SpringBeanOne: BeanName Aware : " + name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("SpringBeanOne: Bean Factory Aware: ");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("SpringBeanOne: Application Context Aware");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("SpringBeanOne: Initializing Bean: Bean is ready for use");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("SpringBeanOne: Destroyed");
+    }
+
+}
