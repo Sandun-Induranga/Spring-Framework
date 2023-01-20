728bca42209fd16654f0f6f2d87df37e019d97dd
Sandun Induranga <sandutharu40@gmail.com>
2023-01-20 16:00:12 +0530

    Mapping Spec

---
+++ _12_Mapping_Spec/src/lk/ijse/spring/controller/FourController.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@RestController
+@RequestMapping("/four")
+public class FourController {
+
+
+    // Character Mapping
+
+    // ?    -> Any Character can assign this in url
+
+    @GetMapping("/C?DEF")
+    public String testOne() {
+        return "Method Invoked";
+    }
+
+}
