3edd656f98bb03ac1e4f5ed3208e3d00199175d9
Sandun Induranga <sandutharu40@gmail.com>
2023-01-20 09:43:12 +0530

    Spring web MVC

--- _09_Spring_Web_MVC/src/lk/ijse/spring/config/WebAppConfig.java
+++ _09_Spring_Web_MVC/src/lk/ijse/spring/config/WebAppConfig.java
@@ -1,8 +1,16 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
+
+@Configuration
+@EnableWebMvc
+//@ComponentScan
 public class WebAppConfig {
 }
--- _09_Spring_Web_MVC/src/lk/ijse/spring/config/WebRootConfig.java
+++ _09_Spring_Web_MVC/src/lk/ijse/spring/config/WebRootConfig.java
@@ -1,8 +1,12 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
+
+@Configuration
 public class WebRootConfig {
 }
