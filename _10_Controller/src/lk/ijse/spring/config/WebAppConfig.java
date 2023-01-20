5b2918a4ebcca2200acdbcd662573f68cace52c8
Sandun Induranga <sandutharu40@gmail.com>
2023-01-20 13:37:13 +0530

    Controller

--- _10_Controller/src/lk/ijse/spring/config/WebAppConfig.java
+++ _10_Controller/src/lk/ijse/spring/config/WebAppConfig.java
@@ -1,8 +1,11 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * @author : Sandun Induranga
@@ -16,6 +19,18 @@ public class WebAppConfig {
 
     public WebAppConfig() {
         System.out.println("WebAppConfig: Instantiated");
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix();
+        viewResolver.setSuffix();
+
+        return viewResolver;
+
     }
 
 }
