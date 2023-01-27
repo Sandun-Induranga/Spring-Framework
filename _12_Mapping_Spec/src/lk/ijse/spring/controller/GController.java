2fd4d354d4189973975d1751235e823b5b110735
Sandun Induranga <sandutharu40@gmail.com>
2023-01-27 09:32:09 +0530

    Content Updated

--- _12_Mapping_Spec/src/lk/ijse/spring/controller/GController.java
+++ _12_Mapping_Spec/src/lk/ijse/spring/controller/GController.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,20 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class GController {
 
     /* There are two parts in a request
-    *       1. Header(Metadata)
-    *       2. Body
-    * */
+     *       1. Header(Metadata)
+     *       2. Body
+     * */
 
 //    @GetMapping
 //    public String testOne() {
 //        return "Method Invoked 1";
 //    }
 
-    @GetMapping(produces = {"Application/json"})
-//    @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public String testTwo() {
-        return "Method Invoked 2";
-    }
+//    @GetMapping(produces = {"Application/json"})
+////    @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+//    public String testTwo() {
+//        return "Method Invoked 2";
+//    }
 
 //    @GetMapping(consumes = {"text/html"})
 ////    @GetMapping(consumes = {MediaType.TEXT_HTML_VALUE})
@@ -35,21 +36,27 @@ public class GController {
 //        return "Method Invoked 3";
 //    }
 
-    @GetMapping(produces = {"text/html"})
-//    @GetMapping(consumes = {MediaType.TEXT_HTML_VALUE})
-    public String testFour() {
-        return "Method Invoked 3";
-    }
+//    @GetMapping(produces = {"text/html"})
+////    @GetMapping(consumes = {MediaType.TEXT_HTML_VALUE})
+//    public String testFour() {
+//        return "Method Invoked 3";
+//    }
 
     /*
-    *       consume    =    Content-Type
-    *       produces   =    Accept - Default (all)
-    *
-    *
-    *
-    *       Content-Type    -    What are the content type of the request body
-    *       Accept    -    What type we want from the response
-    *
-    * */
+     *       consume    =    Content-Type
+     *       produces   =    Accept - Default (all)
+     *
+     *
+     *
+     *       Content-Type    -    What are the content type of the request body
+     *       Accept    -    What type we want from the response
+     *
+     * */
+
+    @GetMapping(produces = {MediaType.TEXT_HTML_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE})
+//    @GetMapping(consumes = {MediaType.TEXT_HTML_VALUE})
+    public String testFour() {
+        return "Method Invoked 4";
+    }
 
 }
