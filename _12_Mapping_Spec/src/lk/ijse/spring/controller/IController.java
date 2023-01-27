6b5212b134f0d0a324d1303c513593a1bbc96885
Sandun Induranga <sandutharu40@gmail.com>
2023-01-27 14:00:52 +0530

    Spring With Maven

--- _12_Mapping_Spec/src/lk/ijse/spring/controller/IController.java
+++ _12_Mapping_Spec/src/lk/ijse/spring/controller/IController.java
@@ -3,6 +3,8 @@ package lk.ijse.spring.controller;
 import lk.ijse.spring.dto.CustomerDTO;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
@@ -14,8 +16,13 @@ import org.springframework.web.bind.annotation.*;
 public class IController {
 
     @PutMapping
-    public void getCustomer(@RequestBody CustomerDTO customerDTO){
-        System.out.println(customerDTO);
+    public ArrayList<CustomerDTO> getCustomer(@RequestBody CustomerDTO customerDTO){
+        ArrayList<CustomerDTO> dtos = new ArrayList<>();
+        dtos.add(customerDTO);
+        dtos.add(customerDTO);
+        dtos.add(customerDTO);
+        dtos.add(customerDTO);
+        return dtos;
     }
 
 //    @RequestBody is a required annotation that use to represent json value
