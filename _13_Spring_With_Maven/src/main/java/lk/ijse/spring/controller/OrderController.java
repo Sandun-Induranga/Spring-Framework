1bf3aa92bb059ab1ba89f8d8996432d04dc28c7a
Sandun Induranga <sandutharu40@gmail.com>
2023-01-30 00:58:48 +0530

    Content Updated

--- _13_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/OrderController.java
+++ _13_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/OrderController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 @CrossOrigin
 public class OrderController {
+
+    @GetMapping
+    public void getAllOrders(){
+
+    }
+
 }
