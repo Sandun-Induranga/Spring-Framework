168a798f15465072a220ec3651f43ca3f4b59f17
Sandun Induranga <sandutharu40@gmail.com>
2023-01-30 12:16:34 +0530

    Created Method getAllCustomerIds()

--- _13_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/OrderController.java
+++ _13_Spring_With_Maven/src/main/java/lk/ijse/spring/controller/OrderController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.db.DB;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @GetMapping
-    public void getAllOrders(){
+    public ResponseUtil getAllOrders() {
+
+        return new ResponseUtil("200", "Successfully Loaded..!", DB.customerDB);
 
     }
 
