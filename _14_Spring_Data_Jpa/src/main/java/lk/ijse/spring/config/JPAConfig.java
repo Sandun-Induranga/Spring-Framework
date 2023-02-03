aa605dd7992c930ad07d0dde61b53d2559e847fc
Sandun Induranga <sandutharu40@gmail.com>
2023-02-03 10:36:22 +0530

    Spring JPA

--- _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,6 +1,12 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
 
 /**
  * @author : Sandun Induranga
@@ -9,5 +15,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter vendorAdapter) {
+
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setPackagesToScan("lk.ijse.spring.entity");
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+
+        return factoryBean;
+
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/db?createDatabaseIfNotExist=true");
+        dataSource.setUsername("sandu");
+        dataSource.setPassword("1234");
+        return dataSource;
+
+    }
 
 }
