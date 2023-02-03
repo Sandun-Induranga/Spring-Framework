31cf1ecacf7b19436dba3c03af3eae0c3f2aa961
Sandun Induranga <sandutharu40@gmail.com>
2023-02-03 10:57:36 +0530

    Spring JPA

---
+++ _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public interface CustomerRepo extends JpaRepository<Customer, String> {
+}
