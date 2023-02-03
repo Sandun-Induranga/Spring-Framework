cf03ea4b3c99c45167dc76660ca101827fa850fe
Sandun Induranga <sandutharu40@gmail.com>
2023-02-03 09:55:09 +0530

    Spring JPA

--- _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/entity/Item.java
+++ _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/entity/Item.java
@@ -1,8 +1,28 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Item {
+    @Id
+    private String code;
+    private String description;
+    private int qtyOnHand;
+    private double unitPrice;
+
 }
