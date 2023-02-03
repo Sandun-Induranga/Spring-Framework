a60d512f686e332404ad2905779b65b1f650c47e
Sandun Induranga <sandutharu40@gmail.com>
2023-02-03 10:43:00 +0530

    Spring JPA

--- _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 
@@ -37,6 +39,18 @@ public class JPAConfig {
         dataSource.setUsername("sandu");
         dataSource.setPassword("1234");
         return dataSource;
+
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.MYSQL);
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQLBDialect");
+        vendorAdapter.setShowSql(true);
+        return vendorAdapter;
 
     }
 
