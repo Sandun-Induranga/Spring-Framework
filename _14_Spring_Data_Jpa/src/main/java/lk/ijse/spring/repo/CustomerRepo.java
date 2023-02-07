00d8414bf5f099707c84b6f6eb9ed8270957faa2
Sandun Induranga <sandutharu40@gmail.com>
2023-02-07 11:46:55 +0530

    Native SQL Queries

--- _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ _14_Spring_Data_Jpa/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -3,6 +3,7 @@ package lk.ijse.spring.repo;
 import lk.ijse.spring.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.ArrayList;
 
@@ -37,10 +38,17 @@ public interface CustomerRepo extends JpaRepository<Customer, String> {
     @Query(value = "SELECT * FROM Customer", nativeQuery = true)
     ArrayList<Customer> methodOne();
 
+
+    // Named Params
     @Query(value = "SELECT * FROM Customer WHERE id='C001'", nativeQuery = true)
     ArrayList<Customer> methodTwo();
 
+    // Positional Params
     @Query(value = "SELECT * FROM Customer WHERE id=?1", nativeQuery = true)
     ArrayList<Customer> methodThree(String id, String name);
+
+    // Name Params
+    @Query(value = "SELECT * FROM Customer WHERE id=:a AND name=:b", nativeQuery = true)
+    ArrayList<Customer> methodFour(@Param("a") String id, @Param("b") String name);
 
 }
