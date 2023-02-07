cf054ccad87f69ca08507c224352dabe21b1a410
Sandun Induranga <sandutharu40@gmail.com>
2023-02-07 12:04:22 +0530

    Tested CustomerServiceImpl

---
+++ _14_Spring_Data_Jpa/src/test/java/lk/ijse/spring/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,55 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.config.WebRootConfig;
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Configuration
+@ContextConfiguration(classes = {WebRootConfig.class})
+@ExtendWith(SpringExtension.class)
+@Transactional
+class CustomerServiceImplTest {
+
+    @Autowired
+    CustomerService customerService;
+
+
+    @Test
+    void saveCustomer() {
+        customerService.saveCustomer(new CustomerDTO("C005","John","England",1000));
+    }
+
+    @Test
+    void updateCustomer() {
+        customerService.updateCustomer(new CustomerDTO("C002","John","America",1000));
+    }
+
+    @Test
+    void deleteCustomer() {
+        customerService.deleteCustomer("C001");
+    }
+
+    @Test
+    void getAllCustomer() {
+        customerService.getAllCustomer();
+    }
+
+    @Test
+    void searchCustomer() {
+        customerService.searchCustomer("Dasun");
+    }
+}
