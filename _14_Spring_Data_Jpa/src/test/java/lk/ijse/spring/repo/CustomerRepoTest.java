ebd837566c5fbb6eaaa2bff4899cbed617192227
Sandun Induranga <sandutharu40@gmail.com>
2023-02-07 10:17:29 +0530

    Spring Testing

--- _14_Spring_Data_Jpa/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ _14_Spring_Data_Jpa/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -1,10 +1,15 @@
 package lk.ijse.spring.repo;
 
+import lk.ijse.spring.config.WebRootConfig;
+import lk.ijse.spring.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,11 +19,26 @@ import static org.junit.jupiter.api.Assertions.*;
  **/
 
 @WebAppConfiguration // Create the testing context
-@ContextConfiguration // Add configuration fot the context
+@ContextConfiguration(classes = {WebRootConfig.class}) // Add configuration fot the context
 @ExtendWith(SpringExtension.class) // Integrate junit with Spring
 class CustomerRepoTest {
 
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Test
+    void findCustomerByName() {
+        ArrayList<Customer> arrayList = customerRepo.findCustomerByName("Dasun");
+        for (Customer customer : arrayList) {
+            System.out.println(customer);
+        }
+    }
+
     @Test
     void findByName() {
+        ArrayList<Customer> arrayList = customerRepo.findByName("Dasun");
+        for (Customer customer : arrayList) {
+            System.out.println(customer);
+        }
     }
 }
